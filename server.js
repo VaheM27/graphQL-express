70df30ed04469c852b496e70114afdb93547f179
VaheM27 <vmnatsakanyan27@gmail.com>
2022-09-12 16:46:00 +0400

    commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const expressGraphQL = require("express-graphql").graphqlHTTP;
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
+const app = express();
+
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "HelloWorld",
+    fields: () => ({
+      message: {
+        type: GraphQLString,
+        resolve: () => "Hello World",
+      },
+    }),
+  }),
+});
+
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema: schema,
+    graphiql: true,
+  })
+);
+app.listen(5000, () => console.log("Server Running"));
